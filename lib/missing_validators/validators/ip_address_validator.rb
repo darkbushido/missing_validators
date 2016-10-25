bd83c66fc110f6f81f1b4eeb3e83e2f394f1d46d
Lance Sanchez <DarkBushido@users.noreply.github.com>
2016-10-25 12:07:11 -0500

    updating the ip_address validator to work with ipaddr

--- lib/missing_validators/validators/ip_address_validator.rb
+++ lib/missing_validators/validators/ip_address_validator.rb
@@ -1,3 +1,4 @@
+require 'ipaddr'
 # Checks if the value of an attribute is a valid IP address.
 #
 # @example Validate that the device IP address is valid.
@@ -8,11 +9,15 @@
 class IpAddressValidator < BaseValidator
   private
 
-  IP_ADDRESS_FORMATS = [
-    /^(([0-9]|[0-9]{2}|1[0-9]{2}|2[0-5][0-9]|2[0-5][0-5])\.){3}([0-9]|[0-9]{2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/    # 192.168.0.10
-  ]
-
   def valid?(ip_address, _)
-    IP_ADDRESS_FORMATS.any? { |format| (ip_address =~ format).present? }
+    begin
+      if ip_address.is_a? IPAddr
+        true
+      else
+        IPAddr.new(ip_address)
+      end
+    rescue IPAddr::InvalidAddressError, IPAddr::AddressFamilyError => e
+      false
+    end
   end
 end
