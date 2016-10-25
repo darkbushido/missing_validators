8f0879789af050ed0beef464b96dbe317d8ca6ed
Lance Sanchez <DarkBushido@users.noreply.github.com>
2016-10-25 11:58:12 -0500

    adding in some testscases

--- spec/validators/ip_address_spec.rb
+++ spec/validators/ip_address_spec.rb
@@ -14,40 +14,10 @@ describe IpAddressValidator do
   it { is_expected.to ensure_valid_ip_address_format_of(:ip) }
   it { is_expected.not_to ensure_valid_ip_address_format_of(:name) }
 
-  # context 'when MAC-address is valid' do
-  #   it { is_expected.to allow_value('08:00:2b:01:02:03').for(:mac) }
-  #   it { is_expected.to allow_value('08-00-2B-01-02-03').for(:mac) }
-  #   it { is_expected.to allow_value('08.00.2b.01.02.03').for(:mac) }
-  #   it { is_expected.to allow_value('08 00 2B 01 02 03').for(:mac) }
-  #   it { is_expected.to allow_value('08002b:010203').for(:mac) }
-  #   it { is_expected.to allow_value('08002B.010203').for(:mac) }
-  #   it { is_expected.to allow_value('08002b-010203').for(:mac) }
-  #   it { is_expected.to allow_value('0800.2b01.0203').for(:mac) }
-  #   it { is_expected.to allow_value('0800-2B01-0203').for(:mac) }
-  #   it { is_expected.to allow_value('0800 2b01 0203').for(:mac) }
-  #   it { is_expected.to allow_value('08002b010203').for(:mac) }
-  # end
-  #
-  # context 'when MAC-address uses different separators' do
-  #   it { is_expected.not_to allow_value('08-00:2b:01:02:03').for(:mac) }
-  #   it { is_expected.not_to allow_value('08.00:2b:01:02:03').for(:mac) }
-  #   it { is_expected.not_to allow_value('08 00:2b:01:02:03').for(:mac) }
-  #   it { is_expected.not_to allow_value('0800-2b01:0203').for(:mac) }
-  #   it { is_expected.not_to allow_value('0800 2B01:0203').for(:mac) }
-  # end
-  #
-  # context 'when MAC-address is too short' do
-  #   it { is_expected.not_to allow_value('08:00:2b:01:02').for(:mac) }
-  #   it { is_expected.not_to allow_value('08-00-2B-01-02').for(:mac) }
-  # end
-  #
-  # context 'when MAC-address is too long' do
-  #   it { is_expected.not_to allow_value('08:00:2b:01:02:03:04').for(:mac) }
-  # end
-  #
-  # context 'when MAC-address includes non-hex digits' do
-  #   it { is_expected.not_to allow_value('qq:00:00:00:00:00').for(:mac) }
-  # end
-  #
-  # it { is_expected.not_to allow_value('invalid').for(:mac) }
+  context 'when IP-Address is valid' do
+    it { is_expected.to allow_value('10.7.177.254').for(:ip) }
+    it { is_expected.to allow_value('8.8.8.8').for(:ip) }
+  end
+
+  it { is_expected.not_to allow_value('invalid').for(:ip) }
 end
